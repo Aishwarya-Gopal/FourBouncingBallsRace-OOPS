7db03b64e8fd4caa289c1a894013b04f3a0cfc4b
aishwarya-g-thoughtworks <aishwarya.g@thoughtworks.com>
2021-05-27 21:49:40 +0530

    Rolled out the second green ball

--- src/main/java/BouncingBallRace.java
+++ src/main/java/BouncingBallRace.java
@@ -2,7 +2,7 @@ import processing.core.PApplet;
 
 public class BouncingBallRace extends PApplet {
     Sketch sketch;
-    Ball ballOne;
+    Ball ballOne, ballTwo;
 
     public static void main(String[] args) {
         PApplet.main("BouncingBallRace", args);
@@ -19,6 +19,7 @@ public class BouncingBallRace extends PApplet {
     public void setup() {
         background(sketch.BACKGROUND_COLOR);
         ballOne = new Ball(235, 52, 52, 0, sketch.HEIGHT / 5, 25, 1);
+        ballTwo = new Ball(79, 255, 56, 0, (sketch.HEIGHT * 2) / 5, 25, 2);
     }
 
     @Override
@@ -26,5 +27,9 @@ public class BouncingBallRace extends PApplet {
         fill(ballOne.RED, ballOne.GREEN, ballOne.BLUE);
         ellipse(ballOne.x_position, ballOne.y_position, ballOne.radius, ballOne.radius);
         ballOne.moveTheBall();
+
+        fill(ballTwo.RED, ballTwo.GREEN, ballTwo.BLUE);
+        ellipse(ballTwo.x_position, ballTwo.y_position, ballTwo.radius, ballTwo.radius);
+        ballTwo.moveTheBall();
     }
 }

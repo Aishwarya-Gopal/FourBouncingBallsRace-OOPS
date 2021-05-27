a9e0828459dd858f1fa2c63b9f3e4362e688a99f
aishwarya-g-thoughtworks <aishwarya.g@thoughtworks.com>
2021-05-27 21:33:00 +0530

    Added the Ball object

---
+++ src/main/java/Ball.java
@@ -0,0 +1,23 @@
+public class Ball {
+    public int RED;
+    public int GREEN;
+    public int BLUE;
+    public int x_position;
+    public double y_position;
+    public int radius;
+    public int velocity;
+
+    public Ball(int RED, int GREEN, int BLUE, int x_position, int y_position, int radius, int velocity) {
+        this.RED = RED;
+        this.GREEN = GREEN;
+        this.BLUE = BLUE;
+        this.x_position = x_position;
+        this.y_position = 0;
+        this.radius = radius;
+        this.velocity = velocity;
+    }
+
+    public void moveTheBall() {
+        this.x_position += this.velocity;
+    }
+}

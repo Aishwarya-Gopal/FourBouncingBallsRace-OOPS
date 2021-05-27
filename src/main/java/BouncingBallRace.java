1c4e9169b2a6e4358445e98f5ec30a738b4e7c59
aishwarya-g-thoughtworks <aishwarya.g@thoughtworks.com>
2021-05-27 21:57:40 +0530

    Rolled out the third ball

--- src/main/java/BouncingBallRace.java
+++ src/main/java/BouncingBallRace.java
@@ -2,7 +2,7 @@ import processing.core.PApplet;
 
 public class BouncingBallRace extends PApplet {
     Sketch sketch;
-    Ball ballOne, ballTwo;
+    Ball ballOne, ballTwo, ballThree;
 
     public static void main(String[] args) {
         PApplet.main("BouncingBallRace", args);
@@ -20,12 +20,14 @@ public class BouncingBallRace extends PApplet {
         background(sketch.BACKGROUND_COLOR);
         ballOne = new Ball(235, 52, 52, 0, sketch.HEIGHT / 5, 25, 1);
         ballTwo = new Ball(79, 255, 56, 0, (sketch.HEIGHT * 2) / 5, 25, 2);
+        ballThree = new Ball(56, 232, 255, 0, (sketch.HEIGHT * 3) / 5, 25, 3);
     }
 
     @Override
     public void draw() {
         setColorAndMove(ballOne);
         setColorAndMove(ballTwo);
+        setColorAndMove(ballThree);
     }
 
     public void setColorAndMove(Ball ball) {

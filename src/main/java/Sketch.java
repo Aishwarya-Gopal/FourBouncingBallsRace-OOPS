d87296877d9dcbebcdba7b3854fb9380b2d82546
aishwarya-g-thoughtworks <aishwarya.g@thoughtworks.com>
2021-05-27 21:28:46 +0530

    Added the Sketch object

---
+++ src/main/java/Sketch.java
@@ -0,0 +1,11 @@
+public class Sketch {
+    public final int WIDTH;
+    public final int HEIGHT;
+    public final int BACKGROUND_COLOR;
+
+    public Sketch(int width, int height, int backgroundColor) {
+        this.WIDTH = width;
+        this.HEIGHT = height;
+        this.BACKGROUND_COLOR = backgroundColor;
+    }
+}

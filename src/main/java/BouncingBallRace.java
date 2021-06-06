f07421de1f7532df1cfd4bb84dff2e19724a1ac6
aishwarya-g-thoughtworks <aishwarya.g@thoughtworks.com>
2021-06-06 12:49:31 +0530

    Creating an array of balls to remove duplication

--- src/main/java/BouncingBallRace.java
+++ src/main/java/BouncingBallRace.java
@@ -1,12 +1,15 @@
 import processing.core.PApplet;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class BouncingBallRace extends PApplet {
 
     Sketch sketch;
     Ball ballOne, ballTwo, ballThree, ballFour;
     Color black, red, green, blue, violet;
+    ArrayList<Ball> balls = new ArrayList<>();
 
     public static void main(String[] args) {
         PApplet.main("BouncingBallRace", args);
@@ -31,15 +34,13 @@ public class BouncingBallRace extends PApplet {
         ballTwo = new Ball(green, 0, setYCoor(2), 25, 2, this);
         ballThree = new Ball(blue, 0, setYCoor(3), 25, 3, this);
         ballFour = new Ball(violet, 0, setYCoor(4), 25, 4, this);
+        Collections.addAll(balls, ballOne, ballTwo, ballThree, ballFour);
     }
 
     @Override
     public void draw() {
         if (!isGameOver()) {
-            ballOne.setColorAndMove();
-            ballTwo.setColorAndMove();
-            ballThree.setColorAndMove();
-            ballFour.setColorAndMove();
+            balls.forEach(Ball::setColorAndMove);
         } else
             exit();
     }
